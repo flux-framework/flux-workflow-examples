97f43c44c4655480968d01deb2cd78365d391205
Christopher Moussa <moussa1@llnl.gov>
2019-12-16 13:45:09 -0800

    Update submitter2.py
    Update new Jobspec format, remove
    RPC calls and get_environment method

--- example2/submitter2.py
+++ example2/submitter2.py
@@ -4,44 +4,24 @@ import json
 import os
 import re
 import flux
-#from flux.rpc import rpc
+from flux.job import JobspecV1
 
-def get_environment():
-    env = dict()
-    for key in os.environ:
-        env[key] = os.environ[key]
-    return env
-
-compute_jobreq = {
-    'nnodes' : 3,
-    'ntasks' : 6,
-    'ncores' : 12,
-    'cmdline' : ["compute.py", "120"],
-    'environ' : get_environment (),
-    'cwd' : os.getcwd (),
-    'walltime' : 0,
-    'ngpus' : 0,
-}
-
-io_jobreq = {
-    'nnodes' : 3,
-    'ntasks' : 3,
-    'ncores' : 3,
-    'cmdline' : ["io-forwarding.py", "120"],
-    'environ' : get_environment (),
-    'cwd' : os.getcwd (),
-    'walltime' : 0,
-    'ngpus' : 0,
-}
-
-payload = json.dumps (compute_jobreq)
 f = flux.Flux ()
-resp = f.rpc_send ("job.submit", payload)
-if resp is None:
-    print "flux.rpc: compute_jobreq", "failed"
 
-payload = json.dumps (io_jobreq)
-resp = f.rpc_send ("job.submit", payload)
-if resp is None:
-    print "flux.rpc: io_jobreq", "failed"
+compute_jobreq = JobspecV1.from_command (
+    command=["./compute.py", "120"],
+    num_tasks=6,
+    num_nodes=3,
+    cores_per_task=2)
+compute_jobreq.cwd = os.getcwd ()
+compute_jobreq.environment = dict (os.environ)
+print (flux.job.submit (f, compute_jobreq))
 
+io_jobreq = JobspecV1.from_command (
+    command=["./io-forwarding.py", "120"],
+    num_tasks=3,
+    num_nodes=3,
+    cores_per_task=1)
+io_jobreq.cwd = os.getcwd ()
+io_jobreq.environment = dict (os.environ)
+print (flux.job.submit (f, io_jobreq))
