61500d740d9fbebee005198884e89c1e060e8729
Christopher Moussa <moussa1@llnl.gov>
2019-12-17 15:09:56 -0800

    Blacken compute, io-forwarding
    Blacken compute.py and io-forwarding.py

--- example7/io-forwarding.py
+++ example7/io-forwarding.py
@@ -3,12 +3,15 @@
 import argparse
 import time
 
-parser = argparse.ArgumentParser (description='forward I/O requests for seconds')
-parser.add_argument ('integer', metavar='S', type=int,
-                     help='an integer for the number of seconds to compute')
+parser = argparse.ArgumentParser(description="forward I/O requests for seconds")
+parser.add_argument(
+    "integer",
+    metavar="S",
+    type=int,
+    help="an integer for the number of seconds to compute",
+)
 
-args = parser.parse_args ()
+args = parser.parse_args()
 
-print "Will forward I/O requests for " + str (args.integer) + " seconds."
-time.sleep (args.integer)
-
+print "Will forward I/O requests for " + str(args.integer) + " seconds."
+time.sleep(args.integer)
