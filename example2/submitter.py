a62c5771d8128b3d0e07450f85a370cf4d4cdc66
Christopher Moussa <moussa1@llnl.gov>
2019-12-17 10:09:02 -0800

    Blacken Python files
    Blacken Python files

--- example2/submitter.py
+++ example2/submitter.py
@@ -6,22 +6,18 @@ import re
 import flux
 from flux.job import JobspecV1
 
-f = flux.Flux ()
+f = flux.Flux()
 
-compute_jobreq = JobspecV1.from_command (
-    command=["./compute.py", "120"],
-    num_tasks=4,
-    num_nodes=2,
-    cores_per_task=2)
-compute_jobreq.cwd = os.getcwd ()
-compute_jobreq.environment = dict (os.environ)
-print (flux.job.submit (f, compute_jobreq))
+compute_jobreq = JobspecV1.from_command(
+    command=["./compute.py", "120"], num_tasks=4, num_nodes=2, cores_per_task=2
+)
+compute_jobreq.cwd = os.getcwd()
+compute_jobreq.environment = dict(os.environ)
+print(flux.job.submit(f, compute_jobreq))
 
-io_jobreq = JobspecV1.from_command (
-    command=["./io-forwarding.py", "120"],
-    num_tasks=1,
-    num_nodes=1,
-    cores_per_task=1)
-io_jobreq.cwd = os.getcwd ()
-io_jobreq.environment = dict (os.environ)
-print (flux.job.submit (f, io_jobreq))
+io_jobreq = JobspecV1.from_command(
+    command=["./io-forwarding.py", "120"], num_tasks=1, num_nodes=1, cores_per_task=1
+)
+io_jobreq.cwd = os.getcwd()
+io_jobreq.environment = dict(os.environ)
+print(flux.job.submit(f, io_jobreq))
--- example2/submitter2.py
+++ example2/submitter2.py
@@ -6,22 +6,18 @@ import re
 import flux
 from flux.job import JobspecV1
 
-f = flux.Flux ()
+f = flux.Flux()
 
-compute_jobreq = JobspecV1.from_command (
-    command=["./compute.py", "120"],
-    num_tasks=6,
-    num_nodes=3,
-    cores_per_task=2)
-compute_jobreq.cwd = os.getcwd ()
-compute_jobreq.environment = dict (os.environ)
-print (flux.job.submit (f, compute_jobreq))
+compute_jobreq = JobspecV1.from_command(
+    command=["./compute.py", "120"], num_tasks=6, num_nodes=3, cores_per_task=2
+)
+compute_jobreq.cwd = os.getcwd()
+compute_jobreq.environment = dict(os.environ)
+print(flux.job.submit(f, compute_jobreq))
 
-io_jobreq = JobspecV1.from_command (
-    command=["./io-forwarding.py", "120"],
-    num_tasks=3,
-    num_nodes=3,
-    cores_per_task=1)
-io_jobreq.cwd = os.getcwd ()
-io_jobreq.environment = dict (os.environ)
-print (flux.job.submit (f, io_jobreq))
+io_jobreq = JobspecV1.from_command(
+    command=["./io-forwarding.py", "120"], num_tasks=3, num_nodes=3, cores_per_task=1
+)
+io_jobreq.cwd = os.getcwd()
+io_jobreq.environment = dict(os.environ)
+print(flux.job.submit(f, io_jobreq))
