be62a026a0221dc6bbcfe216097693795d17e06e
Christopher Moussa <moussa1@llnl.gov>
2020-02-03 09:50:12 -0800

    Fix semantics of bookkeeper.py
    Update command line args to default
    to submitting 6 total jobs:
    3 instances of compute.py
    and 3 instances of io-forwarding.py
    when no arg is specified
    
    When a command line arg N is specified,
    then N/2 compute jobs are submitted, and
    N/2 io-forwarding jobs are submitted
    
    Update README to reflect update to
    semantics

--- example7/bookkeeper.py
+++ example7/bookkeeper.py
@@ -2,28 +2,29 @@
 
 import json
 import os
-import flux
 import argparse
+import sys
+
+import flux
+from flux.job import JobspecV1
 
 compute_jobreq = JobspecV1.from_command(
-    command=["./compute.py", "120"], num_tasks=6, num_nodes=3, cores_per_task=2
+    command=["./compute.py", "10"], num_tasks=6, num_nodes=3, cores_per_task=2
 )
 compute_jobreq.cwd = os.getcwd()
 compute_jobreq.environment = dict(os.environ)
 
 io_jobreq = JobspecV1.from_command(
-    command=["./io-forwarding.py", "120"], num_tasks=3, num_nodes=3, cores_per_task=1
+    command=["./io-forwarding.py", "10"], num_tasks=3, num_nodes=3, cores_per_task=1
 )
 io_jobreq.cwd = os.getcwd()
 io_jobreq.environment = dict(os.environ)
 
-njobs = 0
-
 
 def job_state_cb(f, typemask, message, arg):
     global njobs
-    N = args.integer
-    for jobid, state in message.payload["transitions"]:
+    N = njobs
+    for jobid, state, time in message.payload["transitions"]:
         print("job " + str(jobid) + " changed its state to " + str(state))
         if state == "INACTIVE":
             njobs += 1
@@ -33,10 +34,11 @@ def job_state_cb(f, typemask, message, arg):
 
 # submit bundles of jobs using flux.job.submit ()
 def submit_bundles(f, N):
-    f = flux.Flux()
-    for i in range(0, N):
+    for i in range(0, N // 2):
         print(flux.job.submit(f, compute_jobreq))
         print(flux.job.submit(f, io_jobreq))
+    if N % 2 == 1:
+        print(flux.job.submit(f, compute_jobreq))
 
     print("bookkeeper: all jobs submitted")
 
@@ -54,13 +56,16 @@ def main():
         type=int,
         help="the number of bundles to submit and wait",
     )
-    global args
-    args = parser.parse_args()
+
+    if len(sys.argv) != 2:
+        njobs = 6
+    else:
+        njobs = int(sys.argv[1])
 
     f = flux.Flux()
     f.event_subscribe("job-state")
     f.msg_watcher_create(job_state_cb, 0, "job-state").start()
-    submit_bundles(f, args.integer)
+    submit_bundles(f, njobs)
     print("bookkeeper: waiting until all jobs complete")
     f.reactor_run(f.get_reactor(), 0)
     print("bookkeeper: all jobs completed")
